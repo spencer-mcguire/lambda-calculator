9be102abced09a64392c9b007ff67ea6f8e76080
spencer-mcguire <smcguire1826@gmail.com>
2019-11-05 14:44:15 -0600

    update operator structure

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -9,10 +9,16 @@ import { operators } from "../../../data";
 const Operators = () => {
   // STEP 2 - add the imported data to state
   const [operatorButton] = useState(operators);
+  return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {operatorButton.map((button, index) => (
+        <OperatorButton key={index} obutton={button} />
+      ))}
     </div>
   );
 };
+
+export default Operators;
+/* STEP 3 - Use .map() to iterate over your array data and return a button
+ component matching the name on the provided file. Pass
+ it any props needed by the child component*/
